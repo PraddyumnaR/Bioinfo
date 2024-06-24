288c22098a5d727d139accd92eae0673289cf031
PraddyumnaR <151534093+PraddyumnaR@users.noreply.github.com>
2024-06-24 17:07:15 +0530

    log filterscript.R

--- log filterscript.R
+++ log filterscript.R
@@ -1,8 +1,8 @@
 library(dplyr)
 # Read the DESeq2 results CSV file into a data frame
-# Replace "your_deseq_results.csv" with the actual filename
-deseq_results <- read.csv("Deseq3C result.csv", header = TRUE, row.names = 1)
-# Filter genes based on log fold change values
+# Replace "Deseq3C_result.csv" with the actual filename
+deseq_results <- read.csv("Deseq3C_result.csv", header = TRUE, row.names = 1)
+# Filter genes based on log fold change values here we kept genes showing log fold change value greater than +1 and less than -1
 filtered_results <- deseq_results %>%
   filter(log2FoldChange > 1 | log2FoldChange < -1)
 # Print the first few rows of the filtered results
